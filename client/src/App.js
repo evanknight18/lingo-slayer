8830b2222484ed64335ca135d7b13094533b1102
Evan <evanknight18@gmail.com>
2023-08-03 08:32:39 -0600

    app js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Auth from './components/Auth';
-import Community from './components/Community';
-import Dashboard from './components/Dashboard';
-import Lessons from './components/Lessons';
-import Quizzes from './components/Quizzes';
-import TravelHistory from './components/TravelHistory';
-import Vocabulary from './components/Vocabulary';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  Header,
+  Footer,
+  Dashboard,
+  LessonsPage,
+  LessonDetail,
+  CircleOfFifthsPage,
+  ScalesPage,
+  ChordsPage,
+  QuizPage
+} from './pages';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/auth" component={Auth} />
-          <Route path="/community" component={Community} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/lessons" component={Lessons} />
-          <Route path="/quizzes" component={Quizzes} />
-          <Route path="/travel-history" component={TravelHistory} />
-          <Route path="/vocabulary" component={Vocabulary} />
-        </Switch>
-      </div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/lessons" component={LessonsPage} />
+        <Route path="/lesson-detail/:id" component={LessonDetail} />
+        <Route path="/circle-of-fifths" component={CircleOfFifthsPage} />
+        <Route path="/scales" component={ScalesPage} />
+        <Route path="/chords" component={ChordsPage} />
+        <Route path="/quiz" component={QuizPage} />
+        {/* You can add more routes here as needed */}
+      </Switch>
+      <Footer />
     </Router>
   );
 }
